39fe3de2e44b065e5362772ba7def191b739d153
Alma-tae <taehuikim0430@gmail.com>
2021-04-12 18:22:28 +0900

    MultiMedia

---
+++ supportlibrary/src/main/java/naver/peekaboo21/supportlibrary/Fragment3.java
@@ -0,0 +1,20 @@
+package naver.peekaboo21.supportlibrary;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //fragment3.xml 파일을 자신의 화면으로 사용
+        return inflater.inflate(R.layout.fragment3, container, false);
+    }
+}
